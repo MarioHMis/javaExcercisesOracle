c015b09fe48dbaff277da0d99c67eb6915e5ff95
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-02-05 12:47:56 -0600

    changes name app

--- src/main/java/duke/choice/Customer.java
+++ src/main/java/duke/choice/Customer.java
@@ -1,4 +1,17 @@
 package duke.choice;
 
 public class Customer {
+    private String name;
+    private String size = "M";
+
+    public String getSize() {
+        return size;
+    }
+    public String setSize(String size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
