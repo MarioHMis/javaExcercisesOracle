3514362407680aaea7ee7fed460049146a34df34
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-02-05 13:11:04 -0600

    changes in the project structure

--- src/main/java/duke/choice/Customer.java
+++ src/main/java/duke/choice/Customer.java
@@ -1,17 +1,5 @@
 package duke.choice;
 
 public class Customer {
-    private String name;
-    private String size = "M";
 
-    public String getSize() {
-        return size;
-    }
-    public String setSize(String size) {
-        this.size = size;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
