936da9b649882ab0f78a783796aa406ad43dc7b7
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-02-07 11:36:44 -0600

    adding Customer Class

--- src/main/java/duke/choice/ShopApp.java
+++ src/main/java/duke/choice/ShopApp.java
@@ -3,6 +3,10 @@ package duke.choice;
 
 public class ShopApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Duke Choice Shop!");
+
+        Customer c1 = new Customer();//instance of a class
+        c1.name = "Pinky";
+
+        System.out.println("Customer name is " + c1.name);
     }
 }
