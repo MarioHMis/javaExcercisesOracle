7707c8b09104a0046d375355d36741bbd8ede934
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-02-07 14:12:31 -0600

    adding switch

--- src/main/java/duke/choice/ShopApp.java
+++ src/main/java/duke/choice/ShopApp.java
@@ -4,13 +4,14 @@ package duke.choice;
 public class ShopApp {
     public static void main(String[] args) {
 
-        double tax =0.2;
+        double tax = 0.2;
         double total = 0.0;
 
         System.out.println("Welcome to the Duke Choice Shop");
 
         Customer c1 = new Customer();//instance of a class
         c1.name = "Pinky";
+        c1.size = "S";
 
         System.out.println("Customer name is " + c1.name);
 
@@ -25,11 +26,27 @@ public class ShopApp {
         item2.price = 10.5;
         item2.size = "S";
 
-        System.out.println("Item1 " + item1.description +", " + item1.price + ", " + item1.size );
+        System.out.println("Item1 " + item1.description + ", " + item1.price + ", " + item1.size);
         System.out.println("Item2" + item2.description + ", " + item2.price + ", " + item2.size);
 
-        total = (item1.price + (item2.price * 2)) * (1 + tax) ;
+        total = (item1.price + (item2.price * 2)) * (1 + tax);
         System.out.println("The total is " + total);
 
+        int measurement = 3;
+
+
+        switch (measurement) {
+            case 1, 2, 3:
+                c1.size = "S";
+                break;
+            case 4, 5, 6:
+                c1.size = "M";
+                break;
+            case 7, 8, 9:
+                c1.size = "L";
+                break;
+            default:
+                c1.size = "X";
+        }
     }
 }
