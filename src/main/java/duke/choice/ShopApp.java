3787441da0a1bad9e3f7ce0522cd788872e03dda
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2025-02-21 12:51:16 -0600

    adding try catch tyo handle exceptions

--- src/main/java/duke/choice/ShopApp.java
+++ src/main/java/duke/choice/ShopApp.java
@@ -18,26 +18,11 @@ public class ShopApp {
         System.out.println("Min price " + Clothing.MIN_PRICE);
 
 
-        Clothing item1 = new Clothing("Blue Jacket", 20.9, "M" );
-        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S" );
+        Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
+        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
 
-        Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5.0, "S"  ), new Clothing("Blue T-Shirt", 10.5, "S"  )};
+        Clothing[] items = {item1, item2, new Clothing("Green Scarf", 5.0, "S"), new Clothing("Blue T-Shirt", 10.5, "S")};
 
-
-        item1.setDescription("Blue Jacket");
-        item1.setPrice(20.9);
-
-        item2.setDescription("Orange T-Shirt");
-        item2.setPrice(10.5);
-        item2.setSize("S");
-
-        items[2].setDescription("Green Scarf");
-        items[2].setPrice(5.0);
-        items[2].setSize("S");
-
-        items[3].setDescription("Blue T-Shirt");
-        items[3].setPrice(10.5);
-        items[3].setSize("S");
 
 //        System.out.println("Item1 " + item1.description + ", " + item1.price + ", " + item1.size);
 //        System.out.println("Item2" + item2.description + ", " + item2.price + ", " + item2.size);
@@ -53,10 +38,25 @@ public class ShopApp {
 
         System.out.println("Customer is " + c1.getName() + "," + c1.getSize() + "," + c1.getTotalClothingCost());
 
-        for(Clothing item : c1.getItems()) {
-            System.out.println("Items " + item.getDescription());
+        for (Clothing item : c1.getItems()) {
+            System.out.println("Items " + item.getDescription() + "," + item.getSize() + "." + item.getPrice());
         }
 
+        int average = 0;
+        int count = 0;
 
+        for (Clothing item : c1.getItems()) {
+            if (item.getSize().equals("L")) {
+                count++;
+                average += item.getPrice();
+            }
+        }
+        try {
+            average = average / count;
+            System.out.println("Average price " + average + ", Count " + count);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Don't divide by 0");;
+        }
     }
 }
